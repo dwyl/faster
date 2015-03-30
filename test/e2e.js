1a9366457528a401f09e12c5918ceca174f73855
nelsonic <contact.nelsonic@gmail.com>
2015-03-30 18:31:19 +0100

    exposes the LAN IP Address to the module https://github.com/ideaq/faster/issues/34

--- test/e2e.js
+++ test/e2e.js
@@ -31,13 +31,40 @@ test(cyan('Run Faster. Update a File. Listen for Re-Start Event. Close'), functi
       if(err) {
         console.log(err);
       }
-      setTimeout(function(){
-        socket.disconnect();
-        faster.terminate(function(err, done){
-          t.true(done, green("✓ Cleanup Complete"))
-          t.end();
-        });
-      },2000);
+      // socekt should receive a message here...
+      t.end();
     });
   })
+});
+
+var Wreck = require('wreck');
+
+test(cyan('Access Faster Server style.css and client.js'), function(t){
+  Wreck.get('http://localhost:'+port+'/', function (err, res, payload) {
+    t.equal(payload, '404', "✓ 404");
+  });
+  Wreck.get('http://localhost:'+port+'/style.css', function (err, res, payload) {
+    t.true(payload.indexOf('#fstr_refresh__') > -1, "✓ style.css loaded");
+  });
+  Wreck.get('http://localhost:'+port+'/client.js', function (err, res, payload) {
+    t.true(payload.indexOf('function') > -1, "✓ client.js loaded");
+  });
+  Wreck.get('http://localhost:'+port+'/fail.html', function (err, res, payload) {
+    // console.log(payload);
+    t.equal(payload, '404', "✓ fail should 404");
+    // t.end()
+  });
+  var ip = faster.ip;
+  t.true(ip.length > 8, green("✓ IP Address is: ")+ip);
+  t.end();
+});
+
+test(cyan('Shut Down Faster'), function(t){
+  setTimeout(function(){
+    socket.disconnect();
+    faster.terminate(function(err, done){
+      t.true(done, green("✓ Cleanup Complete"))
+      t.end();
+    });
+  },2000);
 });
